aa9f5549957282b535a070cedeb2eed2b7d705ad
Jack Kamm <jackkamm@gmail.com>
2019-07-28 15:34:50 -0700

    ob-comint-async-R: remove helper functions called only once

--- lisp/ob-comint-async-R.el
+++ lisp/ob-comint-async-R.el
@@ -68,13 +68,18 @@ by `org-babel-comint-async-filter'."
 	 (ess-eval-buffer nil)))
        (with-temp-buffer
 	 (insert
-	  (mapconcat 'org-babel-chomp
-		     (list (org-babel-R-write-last-value-command row-names-p
-								 column-names-p
-								 tmp-file)
-			   (format org-babel-R-async-indicator
-				   "file" tmp-file))
-		     "\n"))
+	  (mapconcat
+           'org-babel-chomp
+           (list (format org-babel-R-write-object-command
+                         (if row-names-p "TRUE" "FALSE")
+                         (if column-names-p
+                             (if row-names-p "NA" "TRUE")
+                           "FALSE")
+                         ".Last.value"
+                         (org-babel-process-file-name tmp-file 'noquote))
+                 (format org-babel-R-async-indicator
+                         "file" tmp-file))
+           "\n"))
 	 (let ((ess-local-process-name
 		(process-name (get-buffer-process session)))
 	       (ess-eval-visibly-p nil))
@@ -100,62 +105,39 @@ Assigned locally to `org-babel-comint-async-chunk-callback' in R
 comint buffers used for asynchronous Babel evaluation."
   (mapconcat
    'org-babel-chomp
-   (cdr (butlast (mapcar (lambda (line) (string-remove-prefix "\n" line))
-			 (org-babel-R-clean-session-output output))))
+   (cdr
+    (butlast
+     (mapcar
+      (lambda (line) (string-remove-prefix "\n" line))
+      (delq nil
+            (mapcar
+             (lambda (line) (when (> (length line) 0) line))
+             (mapcar
+              (lambda (line) ;; cleanup extra prompts left in output
+                (if (string-match
+                     "^\\([>+.]\\([ ][>.+]\\)*[ ]\\)"
+                     (car (split-string line "\n")))
+                    (substring line (match-end 1))
+                  line))
+              output))))))
    "\n"))
 
 (defun org-babel-R-async-value-callback (params tmp-file)
   "Callback for async value results.
 Assigned locally to `org-babel-comint-async-file-callback' in R
 comint buffers used for asynchronous Babel evaluation."
-  (org-babel-R-process-value-result
-   (org-babel-r-value-from-tmp-file
-    (assq :result-params params) tmp-file)
-   ;; TODO this is not exactly the same as colnames-p above...
-   (org-babel-R-get-colnames-p params)))
-
-
-;; helper functions
-
-(defun org-babel-R-value-from-tmp-file (result-params tmp-file)
-  "Insert result from TMP-FILE with RESULT-PARAMS."
-  (org-babel-result-cond result-params
-	  (with-temp-buffer
-	    (insert-file-contents tmp-file)
-	    (org-babel-chomp (buffer-string) "\n"))
-	  (org-babel-import-elisp-from-file tmp-file '(16))))
-
-(defun org-babel-R-clean-session-output (output)
-  "Remove extra prompts and empty lines from OUTPUT."
-  (delq nil
-	(mapcar
-	 (lambda (line) (when (> (length line) 0) line))
-	 (mapcar
-	  (lambda (line) ;; cleanup extra prompts left in output
-	    (if (string-match
-		 "^\\([>+.]\\([ ][>.+]\\)*[ ]\\)"
-		 (car (split-string line "\n")))
-		(substring line (match-end 1))
-	      line))
-	  output))))
-
-(defun org-babel-R-write-last-value-command (row-names-p column-names-p tmp-file)
-  "Generate R command to output last value to TMP-FILE."
-  (format org-babel-R-write-object-command
-	  (if row-names-p "TRUE" "FALSE")
-	  (if column-names-p
-	      (if row-names-p "NA" "TRUE")
-	    "FALSE")
-	  ".Last.value" (org-babel-process-file-name tmp-file 'noquote)))
-
-(defun org-babel-R-get-colnames-p (params)
-  "Determine whether to use column names from PARAMS of R Babel block."
   (let* ((graphics-file (and (member "graphics" (assq :result-params params))
 			     (org-babel-graphical-output-file params)))
 	 (colnames-p (unless graphics-file (cdr (assq :colnames params)))))
-    (or (equal "yes" colnames-p)
-	(org-babel-pick-name
-	 (cdr (assq :colname-names params)) colnames-p))))
+    (org-babel-R-process-value-result
+     (org-babel-result-cond (assq :result-params params)
+       (with-temp-buffer
+         (insert-file-contents tmp-file)
+         (org-babel-chomp (buffer-string) "\n"))
+       (org-babel-import-elisp-from-file tmp-file '(16)))
+     (or (equal "yes" colnames-p)
+	 (org-babel-pick-name
+	  (cdr (assq :colname-names params)) colnames-p)))))
 
 (provide 'ob-comint-async-R)
 
